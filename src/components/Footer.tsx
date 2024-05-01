ca13dfbf6666d1205ce7883316e3343ec93820cc
Luca Di Gianni <luca.dgni@gmail.com>
2024-05-01 15:00:29 +0200

    style: remove footer bg color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-quinary">
+    <footer>
       <div className="container flex flex-col items-center gap-4 py-8">
         <p className="text-2xl font-bold uppercase">Mood</p>
         <p>&copy; 2023 Mood GmbH</p>
