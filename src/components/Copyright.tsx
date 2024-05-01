e6b3c98172b9ed102e6a940291f18d339da5a5a0
Luca Di Gianni <luca.dgni@gmail.com>
2024-05-01 11:24:46 +0200

    style: remove socials and add bg color for copyright

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,18 +1,10 @@
-import { Facebook, Instagram } from "lucide-react";
-
 export default function Copyright() {
   return (
-    <div className="container flex flex-col items-center gap-4 py-8">
-      <div className="flex gap-4">
-        <a href="#">
-          <Facebook size={32} />
-        </a>
-        <a href="#">
-          <Instagram size={32} />
-        </a>
+    <div className="bg-quinary">
+      <div className="container flex flex-col items-center gap-4 py-8">
+        <p className="text-2xl font-bold uppercase">Mood</p>
+        <p>&copy; 2023 Mood GmbH</p>
       </div>
-      <p className="text-2xl font-bold uppercase">Mood</p>
-      <p>&copy; 2023 Mood GmbH</p>
     </div>
   );
 }
