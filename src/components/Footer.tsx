c9181bcf40d1dd1af2d9462cbbfac304aeb65273
Luca Di Gianni <luca.dgni@gmail.com>
2024-05-13 11:49:50 +0200

    style: update footer date

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer>
       <div className="container flex flex-col items-center gap-4 py-8">
         <p className="text-2xl font-bold uppercase">Mood</p>
-        <p>&copy; 2023 Mood GmbH</p>
+        <p>&copy; 2024 Mood GmbH</p>
       </div>
     </footer>
   );
