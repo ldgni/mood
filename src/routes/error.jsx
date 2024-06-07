4f69585bd6e075eecd8d88b0eb5575dadc40faa2
Luca Di Gianni <luca.dgni@gmail.com>
2024-06-07 19:12:17 +0200

    style: update error page

--- src/routes/error.jsx
+++ src/routes/error.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Error() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 p-4 text-center">
       <h1>Sorry, the page you are looking for does not exist.</h1>
       <p>
         <Link
