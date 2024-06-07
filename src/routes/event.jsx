f352e57804c487a1dfa95a4319fd87de8591ddb4
Luca Di Gianni <luca.dgni@gmail.com>
2024-06-07 18:14:44 +0200

    feat: add reusable modal component

--- src/routes/event.jsx
+++ src/routes/event.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import Modal from "../components/ui/modal";
+
 export default function Event() {
   const [showModal, setShowModal] = useState(false);
 
@@ -17,25 +19,12 @@ export default function Event() {
         onClick={() => setShowModal(true)}>
         Preorder now
       </button>
-
-      {showModal && (
-        <div
-          className="fixed inset-0 z-20 flex items-center justify-center bg-gray-500 bg-opacity-75 p-4"
-          role="dialog"
-          aria-modal="true">
-          <div className="flex flex-col items-center gap-2 rounded-2xl bg-slate-600 p-4 text-center text-neutral-300 sm:p-6">
-            <h3 className="font-heading text-2xl font-semibold leading-none sm:text-4xl">
-              Watch out!
-            </h3>
-            <p>Our ordering system is still under construction.</p>
-            <button
-              className="rounded-full bg-neutral-300 px-4 py-2 font-medium text-indigo-950 duration-300 hover:opacity-50"
-              onClick={() => setShowModal(false)}>
-              Close
-            </button>
-          </div>
-        </div>
-      )}
+      <Modal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        title="Watch out!"
+        message="Our ordering system is still under construction."
+      />
     </div>
   );
 }
--- src/routes/franchise.jsx
+++ src/routes/franchise.jsx
@@ -1,5 +1,7 @@
 import { useRef, useState } from "react";
 
+import Modal from "../components/ui/modal";
+
 export default function Franchise() {
   const [showModal, setShowModal] = useState(false);
   const formRef = useRef();
@@ -71,25 +73,12 @@ export default function Franchise() {
           Let&apos;s go!
         </button>
       </form>
-
-      {showModal && (
-        <div
-          className="fixed inset-0 z-20 flex items-center justify-center bg-gray-500 bg-opacity-75 p-4"
-          role="dialog"
-          aria-modal="true">
-          <div className="flex flex-col items-center gap-2 rounded-2xl bg-slate-600 p-4 text-center text-neutral-300 sm:p-6">
-            <h3 className="font-heading text-2xl font-semibold leading-none sm:text-4xl">
-              Success!
-            </h3>
-            <p>Your form has been successfully submitted.</p>
-            <button
-              className="rounded-full bg-neutral-300 px-4 py-2 font-medium text-indigo-950 duration-300 hover:opacity-50"
-              onClick={() => setShowModal(false)}>
-              Close
-            </button>
-          </div>
-        </div>
-      )}
+      <Modal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        title="Success!"
+        message="Your form has been successfully submitted."
+      />
     </div>
   );
 }
