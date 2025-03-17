b564753e1f866b4f1b15ba334dbe3a857ca6e1b1
Luca Di Gianni <luca.dgni@gmail.com>
2025-03-17 12:12:42 +0100

    fix: add overflow-y-scroll to remove layout shift on page change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="overflow-y-scroll">
       <body
         className={`${raleway.className} ${spaceGrotesk.className} bg-amber-50 text-indigo-950`}>
         <main>{children}</main>
